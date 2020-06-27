50d8c93410284ba73b6595d79cead4e8aa74106f
thisistrivial <g.lee13770334@gmail.com>
2020-06-27 00:02:22 -0400

    Added database setup

---
+++ setup/db_handler.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+def create(db_name, *argv):
+  db_path = "../storage/"+ db_name
+  conn = sqlite3.connect(db_path)
+  c = conn.cursor()
+  for arg in argv:
+    arg(c)
+  conn.commit()
+  c.close()
