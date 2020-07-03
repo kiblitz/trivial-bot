9abbecdaed0a2dd52714544d049a9200a5eec9a7
thisistrivial <g.lee13770334@gmail.com>
2020-07-03 00:01:37 -0400

    Restructured methods and added play framework

--- src/bot.py
+++ src/bot.py
@@ -3,21 +3,6 @@ import random
 
 import thanks_handler
 
-async def check_thanks(author, message, channel, mentions):
-  if message[0] != '!thanks':
-    return False
-  if len(message) == 2:
-    message.append("")
-  if (len(mentions) == 0 or 
-     (message[1] != mentions[0].mention and
-      message[1][:2] + message[1][3:] != mentions[0].mention)):
-    await channel.send(thanks_mention_error(author))
-    return True
-  ty_msg = ' '.join(message[2:])
-  thanks_handler.new_thanks(author.id, mentions[0].id, ty_msg)
-  await channel.send(thanks_accepted(author, mentions[0]))
-  return True 
-
 def thanks_mention_error(author):
   possible = ["Make sure to include who you're thanking " + author.mention,
               "Not sure who that's directed to " + author.mention,
@@ -31,30 +16,17 @@ def thanks_accepted(author, to):
   return random.choice(possible)
 
 
-async def check_show(author, message, channel, mentions):
-  if message[0] != '!show':
-    return False 
-  if len(message) == 1:
-    await channel.send(show_empty_error(author)) 
-    return True
-  if ((not await show_thanks(author, message[1:], channel, mentions))):
-    await channel.send(unknown_error(author))
-  return True
-
 def show_empty_error(author):
   possible = ["Not sure what you're trying to show " + author.mention,
               "Make sure to specify what you want to show " + author.mention,
               "Show you what, " + author.mention + "?"] 
   return random.choice(possible)
 
-async def show_thanks(author, message, channel, mentions):
-  if message[0] != 'thanks':
-    return False
-  if len(message) == 1:
-    # TODO show your thanks
-    await channel.send(unimplemented_error(author))
-    return True
-   
+def play_empty_error(author):
+  possible = ["Not sure what you're trying to play " + author.mention,
+              "Make sure to specify what you want to play " + author.mention,
+              "What does " + author.mention + " want to play?"] 
+  return random.choice(possible)
 
 def unknown_error(author):
   possible = ["Not sure I understood that keyword " + author.mention,
@@ -84,8 +56,52 @@ class MyClient(discord.Client):
       return 
 
     if msg_lst[0][0] == '!':
-      (await check_thanks(author, msg_lst, message.channel, message.mentions) or
-      await check_show(author, msg_lst, message.channel, message.mentions))
+      (await self.check_thanks(author, msg_lst, message.channel, message.mentions) or
+      await self.check_show(author, msg_lst, message.channel, message.mentions) or
+      await self.check_play(author, msg_lst, message.channel, message.mentions))
+
+  async def check_thanks(self, author, message, channel, mentions):
+    if message[0] != '!thanks':
+      return False
+    if len(message) == 2:
+      message.append("")
+    if (len(mentions) == 0 or 
+       (message[1] != mentions[0].mention and
+        message[1][:2] + message[1][3:] != mentions[0].mention)):
+      await channel.send(thanks_mention_error(author))
+      return True
+    ty_msg = ' '.join(message[2:])
+    thanks_handler.new_thanks(author.id, mentions[0].id, ty_msg)
+    await channel.send(thanks_accepted(author, mentions[0]))
+    return True 
+
+  async def check_show(self, author, message, channel, mentions):
+    if message[0] != '!show':
+      return False 
+    if len(message) == 1:
+      await channel.send(show_empty_error(author)) 
+      return True
+    if ((not await self.show_thanks(author, message[1:], channel, mentions))):
+      await channel.send(unknown_error(author))
+    return True
+
+  async def show_thanks(self, author, message, channel, mentions):
+    if message[0] != 'thanks':
+      return False
+    if len(message) == 1:
+      # TODO show your thanks
+      await channel.send(unimplemented_error(author))
+      return True
+
+  async def check_play(self, author, message, channel, mentions):
+    if message[0] != '!play':
+      return False
+    if len(message) == 1:
+      await channel.send(play_empty_error(author)) 
+      return True
+    # TODO see check_show
+    await channel.send(unimplemented_error(author))
+    return True
 
 TOKEN = open("../secret/SECRET_TOKEN").read()
 client = MyClient()
